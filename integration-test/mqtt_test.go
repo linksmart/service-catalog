06350840d7f35024cb1b38e573c825cabaea1954
shreekantha Devasya <shreekantha.devasya@fit.fraunhofer.de>
2017-10-18 11:41:42 +0200

    Feature: MQTT support for service registration Status: Fasic functionality and test scripts are working

--- integration-test/mqtt_test.go
+++ integration-test/mqtt_test.go
@@ -19,6 +19,41 @@ type ClientManager struct {
 	c      chan bool
 }
 
+func sameServices(s1, s2 *catalog.Service, checkID bool) bool {
+	// Compare IDs if specified
+	if checkID {
+		if s1.ID != s2.ID {
+			return false
+		}
+	}
+
+	// Compare metadata
+	for k1, v1 := range s1.Meta {
+		v2, ok := s2.Meta[k1]
+		if !ok || v1 != v2 {
+			return false
+		}
+	}
+	for k2, v2 := range s2.Meta {
+		v1, ok := s1.Meta[k2]
+		if !ok || v1 != v2 {
+			return false
+		}
+	}
+
+	// Compare number of protocols
+	if len(s1.ExternalDocs) != len(s2.ExternalDocs) {
+		return false
+	}
+
+	// Compare all other attributes
+	if s1.Description != s2.Description || s1.TTL != s2.TTL {
+		return false
+	}
+
+	return true
+}
+
 func (m *ClientManager) onConnectHandler(client paho.Client) {
 	fmt.Printf("MQTT: %s: Connected.", m.url)
 	m.client = client
@@ -74,14 +109,41 @@ func TestMqtt(t *testing.T) {
 		t.Fatalf("Timed out waiting for broker")
 	}
 	<-manager.c
-	//time.Sleep(5 * time.Second)
+
 	//Publish a service
 	service := MockedService("1")
 	b, _ := json.Marshal(service)
 	manager.client.Publish("LS/MOCK/1/SER/1.0/REG", 1, false, b)
 
-	//create new remote client
-	//	httpClient,_ := data.NewRemoteClient("0.0.0.0:8082",nil)
-	//	services := httpClient.List()
+	time.Sleep(2 * time.Second)
+	//verify if the service is created
+	httpRemoteClient, _ := catalog.NewRemoteCatalogClient("http://localhost:8082", nil)
+	gotService, err := httpRemoteClient.Get(service.ID)
+	if err != nil {
+		t.Fatalf("Error retrieveing the service %s", service.ID)
+		return
+	}
+	if !sameServices(gotService, service, true) {
+		t.Fatalf("The retrieved service is not the same as the added one:\n Added:\n %v \n Retrieved: \n %v", service, gotService)
+	}
+
+	//destroy the service
+	time.Sleep(2 * time.Second)
+
+	manager.client.Publish("LS/MOCK/1/SER/1.0/WILL", 1, false, b)
+
+	//verify if the service is deleted
+	time.Sleep(2 * time.Second)
+	gotService, err = httpRemoteClient.Get(service.ID)
+	if err != nil {
+		switch err.(type) {
+		case *catalog.NotFoundError:
+			break
+		default:
+			t.Fatalf("Error while fetching services:%v", err)
+		}
+	} else {
+		t.Fatalf("Service was fetched even after deletion")
+	}
 
 }
