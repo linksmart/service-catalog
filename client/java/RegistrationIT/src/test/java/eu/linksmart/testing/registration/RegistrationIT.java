8d9831f77df85c091a0459a19cd7a46b2ac844ab
Jose Angel <carvajal@fit.fhg.de>
2017-12-22 14:24:15 +0100

    swagger java client & client IT

---
+++ client/java/RegistrationIT/src/test/java/eu/linksmart/testing/registration/RegistrationIT.java
@@ -0,0 +1,70 @@
+package eu.linksmart.testing.registration;
+
+import io.swagger.client.ApiClient;
+import io.swagger.client.api.ScApi;
+import io.swagger.client.model.APIIndex;
+import io.swagger.client.model.Service;
+import io.swagger.client.model.ServiceDocs;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+/**
+ * Created by José Ángel Carvajal on 21.12.2017 a researcher of Fraunhofer FIT.
+ */
+public class RegistrationIT {
+
+    static final String BASE_URL_PATH = "base_url", BASE_URL = "http://localhost:8082";
+    @Test
+    public void registration(){
+        if(!System.getenv().containsKey("integration_test"))
+            return;
+        ApiClient client = new ApiClient();
+
+        client.setBasePath((System.getenv().containsKey(BASE_URL_PATH))?System.getenv().get(BASE_URL_PATH):BASE_URL);
+
+        ScApi api = new ScApi(client);
+
+        String id = UUID.randomUUID().toString();
+
+        Service service = new Service();
+        service.setName("_it_tcp_");
+        Map<String,String> apis = new HashMap<String, String>();
+        apis.put("Test API","http://test:666");
+        // service.meta("test");
+        service.apis(apis);
+        ServiceDocs docs = new ServiceDocs();
+        docs.addApisItem("Test API");
+        docs.description("it's a test!");
+        docs.setType("application/json");
+        docs.setUrl("http://test:666/docu");
+        service.addDocsItem(docs);
+
+        try{
+            Service service1;
+
+            api.idPut(id,service);
+
+            service1 = api.idGet(id);
+
+            assertTrue("Ids most be equal", id.equals(service1.getId()));
+            APIIndex index =  api.rootGet(new BigDecimal(1),new BigDecimal(100));
+            assertTrue("It must contain 1 service", index.getTotal().equals(1));
+            api.idDelete(id);
+
+        }catch (Exception e){
+            e.printStackTrace();
+            fail();
+        }
+
+
+
+    }
+}
---
+++ client/java/clientRelease.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+####################################
+## Note: generate client
+## if already exists: rm -R client
+# mvn install -f generate.xml
+####################################
+
+## Adjust pom.xml
+
+sed -i '0,/<groupId>io.swagger<\/groupId>/ s/<groupId>io.swagger<\/groupId>/<groupId>eu.linksmart.tools.registration<\/groupId>/' 'client/pom.xml';
+sed -i '0,/<artifactId>swagger-java-client<\/artifactId>/ s/<artifactId>swagger-java-client<\/artifactId>/<artifactId>service-catalog-client<\/artifactId>/' 'client/pom.xml';
+sed -i '0,/<name>swagger-java-client<\/name>/ s/<name>swagger-java-client<\/name>/<name>service-catalog-client<\/name>/' 'client/pom.xml';
+sed -i s/'<version>1.0.0<\/version>'/'<version>'`jq -r .info.version ../../apidoc/swagger.json`'<\/version>'/ 'client/pom.xml' ;
+
+#######################################################################################################################################################################################################################
+## Note: deploy the client
+#mvn -s D:\.m2\settings.xml deploy:deploy-file -D"pomFile=pom.xml" -D"file=target\swagger-java-client-2.2.x.jar"  -D"repositoryId=releases" -"Durl=https://nexus.linksmart.eu/repository/maven-releases/";
+#######################################################################################################################################################################################################################
