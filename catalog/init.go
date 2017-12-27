9046577fbd9600c4d9bd1c7cf342d66e365609a2
Farshid Tavakolizadeh <email@farshid.ws>
2017-12-27 14:49:58 +0100

    added debugging logger for paho mqtt client, updated dependency

--- catalog/init.go
+++ catalog/init.go
@@ -3,6 +3,10 @@
 package catalog
 
 import (
+	"log"
+	"os"
+
+	paho "github.com/eclipse/paho.mqtt.golang"
 	"github.com/farshidtz/elog"
 )
 
@@ -12,4 +16,12 @@ func init() {
 	logger = elog.New(LoggerPrefix, &elog.Config{
 		DebugPrefix: LoggerPrefix,
 	})
+
+	if os.Getenv("PAHO_DEBUG") == "1" {
+		w := elog.NewWriter(os.Stdout)
+		paho.ERROR = log.New(w, "[paho-error] ", 0)
+		paho.CRITICAL = log.New(w, "[paho-crit] ", 0)
+		paho.WARN = log.New(w, "[paho-warn] ", 0)
+		paho.DEBUG = log.New(w, "[paho-debug] ", 0)
+	}
 }
