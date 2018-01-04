a6bf7b732a643e1d1d0d67beced47050d12e8314
Farshid Tavakolizadeh <email@farshid.ws>
2018-01-04 15:30:25 +0100

    disable debug trace, added paho logging

--- init.go
+++ init.go
@@ -3,7 +3,11 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"code.linksmart.eu/sc/service-catalog/catalog"
+	paho "github.com/eclipse/paho.mqtt.golang"
 	"github.com/farshidtz/elog"
 )
 
@@ -12,5 +16,14 @@ var logger *elog.Logger
 func init() {
 	logger = elog.New(catalog.LoggerPrefix, &elog.Config{
 		DebugPrefix: catalog.LoggerPrefix,
+		DebugTrace:  elog.NoTrace,
 	})
+
+	if os.Getenv("PAHO_DEBUG") == "1" {
+		w := elog.NewWriter(os.Stdout)
+		paho.ERROR = log.New(w, "[paho-error] ", 0)
+		paho.CRITICAL = log.New(w, "[paho-crit] ", 0)
+		paho.WARN = log.New(w, "[paho-warn] ", 0)
+		paho.DEBUG = log.New(w, "[paho-debug] ", 0)
+	}
 }
