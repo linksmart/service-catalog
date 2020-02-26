84280eed363ca0d169a4c224ee9e445dd74aa356
Rohit Deshmukh <rohitadeshmukh13@gmail.com>
2020-02-26 13:38:01 +0100

    removed the custom adjustment of TTL

--- catalog/controller.go
+++ catalog/controller.go
@@ -47,12 +47,7 @@ func (c *Controller) add(s Service) (*Service, error) {
 	s.Created = time.Now().UTC()
 	s.Updated = s.Created
 
-	if s.TTL == 0 {
-		// No TTL or TTL=0 in request payload implies a TTL of 10 years
-		s.Expires = s.Created.AddDate(10, 0, 0)
-	} else {
-		s.Expires = s.Created.Add(time.Duration(s.TTL) * time.Second)
-	}
+	s.Expires = s.Created.Add(time.Duration(s.TTL) * time.Second)
 
 	err := c.storage.add(&s)
 	if err != nil {
